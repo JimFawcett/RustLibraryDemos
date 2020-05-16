9962e1275a9e3e368fd0fa15383c624e2aa0bfaa
James W. Fawcett <jfawcett@twcny.rr.com>
2020-05-16 12:51:40 -0400

    added thread demo

---
+++ thread_shared/target/debug/deps/rust_thread_probe.d
@@ -0,0 +1,5 @@
+C:\github\JimFawcett\RustLibraryDemos\thread_shared\target\debug\deps\rust_thread_probe.exe: src\main.rs
+
+C:\github\JimFawcett\RustLibraryDemos\thread_shared\target\debug\deps\rust_thread_probe.d: src\main.rs
+
+src\main.rs:
---
+++ thread_shared/target/debug/rust_thread_probe.d
@@ -0,0 +1,1 @@
+C:\github\JimFawcett\RustLibraryDemos\thread_shared\target\debug\rust_thread_probe.exe: C:\github\JimFawcett\RustLibraryDemos\thread_shared\src\main.rs
