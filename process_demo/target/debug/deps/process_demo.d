19f279feb670ff8352360e32c16fd23f67c2b1c1
James W. Fawcett <jfawcett@twcny.rr.com>
2020-05-16 20:28:55 -0400

    added process demo

--- process_demo/target/debug/deps/process_demo.d
+++
@@ -1,5 +0,0 @@
-C:\github\JimFawcett\RustLibraryDemos\process_demo\target\debug\deps\process_demo.exe: src\main.rs
-
-C:\github\JimFawcett\RustLibraryDemos\process_demo\target\debug\deps\process_demo.d: src\main.rs
-
-src\main.rs:
--- process_demo/target/debug/process_demo.d
+++
@@ -1,1 +0,0 @@
-C:\github\JimFawcett\RustLibraryDemos\process_demo\target\debug\process_demo.exe: C:\github\JimFawcett\RustLibraryDemos\process_demo\src\main.rs
